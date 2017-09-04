710018b7e9e3edf5018a219620905e4646365ba5
Michael Wellner <michael.wellner@de.ibm.com>
2017-09-05 00:14:51 +0200

    Improvements on validator util.

--- actions/logger/index.js
+++ actions/logger/index.js
@@ -18,28 +18,31 @@ exports.main = (params) => {
     });
   });
 
-  return bot.util.validate(validator, () => {
-    const message = params.message;
-    const messagelevel = params.level;
+  return bot.util
+    .validate(validator)
+    .then(() => {
+      const message = params.message;
+      const messagelevel = params.level;
 
-    if (_.indexOf(LEVELS, params.level) >= _.indexOf(LEVELS, params.config.logger.level)) {
-      return bot.db
-        .create({
-          type: 'log',
-          user: _.get(params, 'payload.conversationcontext.user.id'),
-          payload: _.get(params, 'payload.id'),
-          level: params.level,
-          message: params.message
-        })
-        .then(result => ({
+      if (_.indexOf(LEVELS, params.level) >= _.indexOf(LEVELS, params.config.logger.level)) {
+        return bot.db
+          .create({
+            type: 'log',
+            user: _.get(params, 'payload.conversationcontext.user.id'),
+            payload: _.get(params, 'payload.id'),
+            level: params.level,
+            message: params.message
+          })
+          .then(result => ({
+            statusCode: 200,
+            message: `Message inserted into log.`
+          }));
+      } else {
+        return Promise.resolve({
           statusCode: 200,
-          message: `Message inserted into log.`
-        }));
-    } else {
-      return Promise.resolve({
-        statusCode: 200,
-        message: `Message log level '${params.level}' below configured log level '${params.config.logger.level}'.`
-      });
-    }
-  });
+          message: `Message log level '${params.level}' below configured log level '${params.config.logger.level}'.`
+        });
+      }
+    })
+    .catch(bot.util.defaultErrorHandler);
 };
