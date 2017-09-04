b466b6577faa36daa3c8c6b22e91ca9117f31f30
Michael Wellner <michael.wellner@de.ibm.com>
2017-09-04 10:56:38 +0200

    Added tests for logger.

--- actions/logger/index.js
+++ actions/logger/index.js
@@ -9,11 +9,11 @@ exports.main = (params) => {
 
   const validator = new Validator();
   validator(params).required().isObject(obj => {
-    obj('message').required.isString();
-    obj('level').required.isString();
+    obj('message').required().isString();
+    obj('level').required().isString();
     obj('config').required().isObject(obj => {
       obj('logger').required().isObject(obj => {
-        obj('level').required.isString();
+        obj('level').required().isString();
       });
     });
   });
@@ -22,7 +22,7 @@ exports.main = (params) => {
     const message = params.message;
     const messagelevel = params.level;
 
-    if (_.indexOf(LEVELS, params.level) >= _.indexOf(LEVELS, params.config.level)) {
+    if (_.indexOf(LEVELS, params.level) >= _.indexOf(LEVELS, params.config.logger.level)) {
       return bot.db
         .create({
           type: 'log',
