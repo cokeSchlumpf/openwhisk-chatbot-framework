bb32144be2a2d95109edb314772769291ddac02a
Michael Wellner <michael.wellner@de.ibm.com>
2017-09-13 00:11:07 +0200

    sc.

--- build.travis.sh
+++ build.travis.sh
@@ -6,7 +6,7 @@ sed -i 's/${CLOUDANT_URL}/'$CLOUDANT_URL'/g' package.parameters.json
 sed -i 's/${CONVERSATION_USERNAME}/'$CONVERSATION_USERNAME'/g' package.parameters.json
 sed -i 's/${CONVERSATION_PASSWORD}/'$CONVERSATION_PASSWORD'/g' package.parameters.json
 sed -i 's/${CONVERSATION_WORKSPACEID}/'$CONVERSATION_WORKSPACEID'/g' package.parameters.json
-cat package.parameters.template.json
+cat package.parameters.json
 
 # install and configure Openwhisk CLI
 wget https://openwhisk.ng.bluemix.net/cli/go/download/linux/amd64/wsk
@@ -15,4 +15,4 @@ export PATH=$PATH:$(pwd)
 wsk property set --apihost $OPENWHISK_APIHOST --auth $OPENWHISK_AUTHKEY
 
 # deploy service
-./build.deploy.sh
+#./build.deploy.sh
