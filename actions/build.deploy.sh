e490d5593689dc765019a0b5455c37c6bc8c1cf6
Michael Wellner <michael.wellner@de.ibm.com>
2017-09-12 23:50:01 +0200

    Removed filter.

--- actions/build.deploy.sh
+++ actions/build.deploy.sh
@@ -10,7 +10,7 @@ echo "Creating Packages '${PACKAGE_GENERIC}' && '${PACKAGE_API}'"
 wsk package create ${PACKAGE_GENERIC} &> /dev/null || true
 wsk package create ${PACKAGE_API}  &> /dev/null || true
 
-for dir in `find . -maxdepth 1 -mindepth 1 -type d | grep -v _template | grep core-input`
+for dir in `find . -maxdepth 1 -mindepth 1 -type d | grep -v _template`
 do
     ACTION=`echo $dir | awk -F'/' '{ print $2 }'`;
 
