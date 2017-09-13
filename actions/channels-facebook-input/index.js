16a5e07e40d6489e879d1ed8003720f8fe9b29c0
Michael Wellner <michael.wellner@de.ibm.com>
2017-09-13 22:48:41 +0200

    sc

--- actions/channels-facebook-input/index.js
+++ actions/channels-facebook-input/index.js
@@ -2,7 +2,7 @@ const _ = require('lodash');
 
 exports.main = (params) => {
   const body = _.get(params, 'request.body', {})
-  console.log(body);
+  console.log(_.keys(body));
 
   if (body['hub.mode'] === 'subscribe' && body['hub.verify_token'] === _.get(params, 'config.facebook.verify_token')) {
     return {
