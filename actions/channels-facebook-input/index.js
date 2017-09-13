3b67b627d399ebdc578120d5d1d8dcb004f450f3
Michael Wellner <michael.wellner@de.ibm.com>
2017-09-13 22:30:53 +0200

    Small changes ...

--- actions/channels-facebook-input/index.js
+++ actions/channels-facebook-input/index.js
@@ -4,7 +4,7 @@ exports.main = (params) => {
   const body = _.get(params, 'request.body', {})
   console.log(body);
 
-  if (body['hub.mode'] === 'subscribe') {
+  if (body['hub.mode'] === 'subscribe' && body['hub.verify_token'] === _.get(params, 'config.facebook.verify_token')) {
     return {
       statusCode: 204,
       response: {
