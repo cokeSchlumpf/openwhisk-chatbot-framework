cbcf6a7d1466a72110f5d86bc1cc4d152569631b
Michael Wellner <michael.wellner@de.ibm.com>
2017-09-14 22:03:03 +0200

    Added Watson Conversation Middleware

---
+++ actions/middleware-wcs/index.js
@@ -0,0 +1,19 @@
+const botpack = require('serverless-botpack-lib');
+
+exports.main = (params) => {
+  const bot = botpack(params);
+
+  bot.log.info(params);
+
+  return new Promise((resolve, reject) => {
+    bot.send('#hello')
+      .then(payload => {
+        bot.send('#howdy').then(payload => {
+          resolve({
+            statusCode: 200,
+            payload: payload
+          });
+        });
+      });
+  });
+}
---
+++ actions/middleware-wcs/index.test.js
@@ -0,0 +1,9 @@
+const _ = require('lodash');
+const chai = require('chai');
+const requireMock = require('mock-require');
+
+describe('middleware-test', () => {
+  it('is just a stupid test', () => {
+    chai.expect(true).to.be.true;
+  });
+});
