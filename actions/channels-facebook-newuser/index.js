8053a346c828bcbe249f7290fa2974ad893426df
Michael Wellner <michael.wellner@de.ibm.com>
2017-09-30 12:05:48 +0200

    Added newuser extension point and channels-facebook-newuser

---
+++ actions/channels-facebook-newuser/index.js
@@ -0,0 +1,41 @@
+const _ = require('lodash');
+const rp = require('request-promise');
+
+exports.main = (params) => {
+  const user = params.user;
+  const accessToken = _.get(params, 'config.facebook.access_token');
+
+  const options = {
+    uri: `https://graph.facebook.com/v2.6/${user.facebook_id}`,
+    qs: {
+      access_token: accessToken
+    },
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json: true 
+  };
+
+  return rp(options)
+    .then(({ first_name, last_name, profile_pic, locale, timezone, gender }) => {
+      return {
+        statusCode: 200,
+        user: _.assign(user, {
+          first_name,
+          last_name,
+          locale,
+          timezone,
+          gender
+        })
+      }
+    })
+    .catch(error => ({
+      statusCode: 503,
+      error: {
+        message: `There was an error fetching the user data from Facebook`,
+        parameters: {
+          error
+        }
+      }
+    }));
+}
