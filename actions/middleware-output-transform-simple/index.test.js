6c7f371b27a26c73ff723f4f5f4b2edf04932dd4
Michael Wellner <michael.wellner@de.ibm.com>
2017-12-03 13:46:32 +0100

    sc.

--- actions/middleware-output-transform-simple/index.test.js
+++ actions/middleware-output-transform-simple/index.test.js
@@ -6,7 +6,7 @@ const sinon = require('sinon');
 describe('middleware-output-transform-simple', () => {
   it('selects a random value if a message is an array', () => {
     const config = {
- 
+
     };
 
     const payload = {
@@ -34,7 +34,7 @@ describe('middleware-output-transform-simple', () => {
 
   it('transforms messages sequences', () => {
     const config = {
- 
+
     };
 
     const payload = {
@@ -71,6 +71,32 @@ describe('middleware-output-transform-simple', () => {
       chai.expect(result.payload.context.output.messages[2].typing_on).to.be.true;
       chai.expect(result.payload.context.output.messages[3]).to.equal('message 2');
       chai.expect(result.payload.context.output.messages[4]).to.be.oneOf(['1', '2', '3']);
+    });
+  });
+
+  it('selects does not randomly select a value if context.messages is an array of spring - it\'s considered to be a sequence', () => {
+    const config = {
+
+    };
+
+    const payload = {
+      context: {
+        output: {
+          channel: 'facebook',
+          messages: [
+            "a",
+            "b",
+            "c"
+          ]
+        }
+      }
+    }
+
+    return require('./index').main({ payload, config }).then(result => {
+      chai.expect(result.statusCode).to.equal(200);
+      chai.expect(result.payload.context.output.messages[0]).to.equal('a');
+      chai.expect(result.payload.context.output.messages[1]).to.equal('b');
+      chai.expect(result.payload.context.output.messages[2]).to.equal('c');
     });
   });
 });
