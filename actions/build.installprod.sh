47d771d23cfe2af5b5fd4144105d6ffa856d2d51
Michael Wellner <michael.wellner@de.ibm.com>
2018-01-04 21:53:46 +0100

    Now using openwhisk-action-manager for deployment.

--- actions/build.installprod.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for dir in `find . -maxdepth 1 -mindepth 1 -type d | grep -v _template`
-do
-    pushd ${dir} > /dev/null
-      echo "Installing dependencies for '${dir}' ..."
-      npm install --only=production
-    popd > /dev/null
-done
