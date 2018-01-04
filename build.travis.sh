82349bdc5e9b902742a899be0349789fb1b5b4f5
Michael Wellner <michael.wellner@de.ibm.com>
2018-01-04 21:58:11 +0100

    Changed build script.

--- build.travis.sh
+++ build.travis.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-cd ./actions
-cp package.parameters.template.json package.parameters.json
-sed -i 's/${CLOUDANT_PASSWORD}/'$CLOUDANT_PASSWORD'/g' package.parameters.json
-sed -i 's/${CONVERSATION_USERNAME}/'$CONVERSATION_USERNAME'/g' package.parameters.json
-sed -i 's/${CONVERSATION_PASSWORD}/'$CONVERSATION_PASSWORD'/g' package.parameters.json
-sed -i 's/${CONVERSATION_WORKSPACEID}/'$CONVERSATION_WORKSPACEID'/g' package.parameters.json
-
 # install and configure Openwhisk CLI
 wget https://openwhisk.ng.bluemix.net/cli/go/download/linux/amd64/wsk
 chmod 777 wsk
 export PATH=$PATH:$(pwd)
 wsk property set --apihost $OPENWHISK_APIHOST --auth $OPENWHISK_AUTHKEY
 
-#deploy service
-./build.deploy.sh
+# deploy the framework
+npm install
+npm run deploy
