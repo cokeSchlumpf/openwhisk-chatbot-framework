eb6379e107cc8d3f0cbbe74b97c8a753505e1674
Michael Wellner <michael.wellner@de.ibm.com>
2018-01-07 18:14:54 +0100

    Allow to send conversationcontext and context with message.

--- actions/channels-http-input/index.js
+++ actions/channels-http-input/index.js
@@ -7,6 +7,8 @@ exports.main = (params = {}) => {
       input: {
         user: _.get(params, 'request.body.user', 'anonymous'),
         message: _.get(params, 'request.body.message'),
+        context: _.get(params, 'request.body.context'),
+        conversationcontext: _.get(params, 'request.body.conversationcontext'),
         sync: true
       }
     };
